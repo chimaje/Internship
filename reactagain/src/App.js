6d661d1af21cfc75f8b39737dba33267a509c751
chimaje <chimaje.agada@pau.edu.ng>
2022-08-09 15:57:08 +0100

    project

--- reactagain/src/App.js
+++ reactagain/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './navbar';
+import Home from './Home';
 
 function App() {
+  // const title='welcome to the new blog';
+  // const likes=50;
+  // const link = "http://www.google.com";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Home/>
+        {/* <h1>App component</h1>
+        <h1> {title}</h1>
+        <p>Liked {likes} times</p>
+
+        <p>{10}</p>
+        <p>{"heellllooo"}</p>
+        <p>{[1,23,45,67]}</p>
+        <p>{Math.random()*10}</p>
+        <a href={link}>Google Site</a> */}
+
+      </div>
     </div>
   );
 }
