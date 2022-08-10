44927079fe2d652b3c1c3559a05a970e06ab7a35
chimaje <chimaje.agada@pau.edu.ng>
2022-08-10 14:31:38 +0100

    project

--- reactagain/src/App.js
+++ reactagain/src/App.js
@@ -1,28 +1,41 @@
 import './App.css';
 import Navbar from './navbar';
 import Home from './Home';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   // const title='welcome to the new blog';
   // const likes=50;
   // const link = "http://www.google.com";
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home/>
-        {/* <h1>App component</h1>
-        <h1> {title}</h1>
-        <p>Liked {likes} times</p>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/create">
+              <Create/>
+            </Route>
+          </Switch>
+          {/* <h1>App component</h1>
+          <h1> {title}</h1>
+          <p>Liked {likes} times</p>
 
-        <p>{10}</p>
-        <p>{"heellllooo"}</p>
-        <p>{[1,23,45,67]}</p>
-        <p>{Math.random()*10}</p>
-        <a href={link}>Google Site</a> */}
+          <p>{10}</p>
+          <p>{"heellllooo"}</p>
+          <p>{[1,23,45,67]}</p>
+          <p>{Math.random()*10}</p>
+          <a href={link}>Google Site</a> */}
 
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- reactagain/src/hooks/useFetch.js
+++ reactagain/src/hooks/useFetch.js
@@ -4,8 +4,9 @@ const useFetch=(url)=>{
     const[isPending,setIspending]=useState(true);
     const [data,setData] = useState(null);
     useEffect(()=>{
-        setInterval(()=>{
-            fetch(url)
+        const abort = new AbortController();
+        setTimeout(()=>{
+            fetch(url,{signal:abort.signal})
                 .then(res=>{
                     console.log(res);
                     if(!res.ok){
@@ -20,11 +21,16 @@ const useFetch=(url)=>{
                     setError(null);
                 })
                 .catch((err)=>{
-                    setIspending(false);
-                    setError(err.message);
+                    if(err.name ==='AbortError'){
+                        console.log('fetch aborted');
+                    }else{
+                        setIspending(false);
+                        setError(err.message);
+                    }
                 })
         },1000);
-    },[url]);
+        return()=> abort.abort();
+     },[url]);
     return{
         data,isPending,error
     }
