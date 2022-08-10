2b28f80864c9b58888185b49fc8a83dad2b1b83a
chimaje <chimaje.agada@pau.edu.ng>
2022-08-10 15:37:09 +0100

    project

--- reactagain/src/bloglist.js
+++ reactagain/src/bloglist.js
@@ -6,7 +6,7 @@ const Bloglist = ({blogger,title,handleDelete}) => {
             <h2>{title}</h2>
         {blogs.map((blog)=>(
             <div className="blog_preview" key={blog.id}>
-                <Link to={`/blogs/${blog.id}`}>
+                <Link classname="an"to={`/blogs/${blog.id}`}>
                 <h3>{blog.title}</h3>
                 <p> Written by {blog.author}</p>
                 </Link>
